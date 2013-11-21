cf4d1e9cf91a9a3b43a374bc0c057993ac991f1f
Laura Lindzey <lindzey@gmail.com>
2013-11-21 15:56:27 -0800

    whoops. forgot to remove deleted script from setup.py

--- clear_table/setup.py
+++ clear_table/setup.py
@@ -5,7 +5,7 @@ d = generate_distutils_setup(
     packages=['clear_table'],
     package_dir={'': 'src'},
     requires=['rospy', 'moveit_msgs', 'sensor_msgs', 'shape_msgs', 'shared_autonomy_msgs', 'smach', 'smach_ros'],
-    scripts=['scripts/clear_table_moveit.py', 'scripts/clear_table3.py']
+    scripts=['scripts/clear_table_moveit.py']
 )
 
 setup(**d)
