2807efe401599ac4a085e70567c65c640e9e5162
Laura Lindzey <lindzey@gmail.com>
2013-11-22 15:46:00 -0800

    clear_table_moveit now works w/ cluster_grasp_planner and moveit-msgs

--- clear_table/setup.py
+++ clear_table/setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['clear_table'],
     package_dir={'': 'src'},
-    requires=['rospy', 'moveit_msgs', 'sensor_msgs', 'shape_msgs', 'shared_autonomy_msgs', 'smach', 'smach_ros'],
+    requires=['rospy', 'moveit_msgs', 'sensor_msgs', 'shape_msgs', 'shared_autonomy_msgs', 'smach', 'smach_ros', 'cluster_grasp_planner'],
     scripts=['scripts/clear_table_moveit.py']
 )
 
