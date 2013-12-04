58894204d886663ec53ac68866b18e645e04f708
Dejan Pangercic <dejan.pangercic@gmail.com>
2013-12-03 21:55:54 -0800

    first stub of ork object actinolib server. todo: points in RecognizedObjectArray are not in camera frame but in table frame

---
+++ scripts/ork_tabletop_actionlib_server.py
@@ -0,0 +1,56 @@
+import rospy
+import tf
+#from std_msgs.msg import String
+from object_recognition_msgs.msg import RecognizedObjectArray
+from sensor_msgs.msg import PointCloud2
+from geometry_msgs.msg import PoseStamped
+from visualization_msgs.msg import MarkerArray
+
+pub = rospy.Publisher('/recognized_object_array_as_point_cloud', PointCloud2)
+pose_pub = rospy.Publisher('/recognized_object_array_as_pose_stamped', PoseStamped)
+br = tf.TransformBroadcaster()
+
+def callback(data):
+    rospy.loginfo("Objects %d", data.objects.__len__())
+
+    for i in range (data.objects.__len__()):
+#        rospy.loginfo("Point clouds %d", data.objects[i].point_clouds.__len__())
+        rospy.loginfo("Objects frame %s", data.objects[i].header.frame_id)
+        rospy.loginfo("Objects pose frame %s", data.objects[i].pose.header.frame_id)
+
+        #table pose
+        pose = PoseStamped()
+        ma = rospy.wait_for_message("/marker_table_origins", MarkerArray);
+        rospy.loginfo("Markers size %d", ma.markers.__len__())
+        if ma.markers.__len__() > 1:
+            pose.header = ma.markers[0].header
+            pose.header.stamp = rospy.Time.now()
+            pose.pose = ma.markers[0].pose
+            pose_pub.publish(pose)
+        else:
+            continue
+
+        
+        #table to camera transform
+        br.sendTransform((pose.pose.position.x, pose.pose.position.y, pose.pose.position.z),
+                        (pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z, pose.pose.orientation.w),
+                         rospy.Time.now(),
+                         "table_link",
+                         ma.markers[0].header.frame_id)
+
+        #transform point cloud
+        pc = data.objects[i].point_clouds[0]
+        #        pc.header.frame_id = 'head_mount_kinect_rgb_optical_frame'
+        pc.header.frame_id = 'table_link'
+        pc.header.stamp = rospy.Time.now()
+        pub.publish(pc)
+        
+
+def listener():
+    rospy.init_node('ork_tabletop_actionlib_server', anonymous=True)
+    rospy.Subscriber("/recognized_object_array", RecognizedObjectArray, callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
