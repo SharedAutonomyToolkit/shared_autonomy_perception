a1aee89933af20d422a9395b73911c050546173d
Laura Lindzey <lindzey@gmail.com>
2013-12-06 10:38:48 -0800

    fixing setup.py to have the correct script name s.t. catkin_make actually works

--- ork_tabletop_actionlib_server/setup.py
+++ ork_tabletop_actionlib_server/setup.py
@@ -5,7 +5,7 @@ d = generate_distutils_setup(
     packages=['ork_tabletop_actionlib_server'],
     package_dir={'': 'src'},
     requires=['actionlib', 'actionlib_msgs', 'geometry_msgs', 'rospy', 'tf', 'object_recognition_msgs', 'sensor_msgs', 'visualization_msgs', 'shared_autonomy_msgs'],
-    scripts=['scripts/actionlib_server.py']
+    scripts=['scripts/ork_tabletop_actionlib_server.py']
 )
 
 setup(**d)
