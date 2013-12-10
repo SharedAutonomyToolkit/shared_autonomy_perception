5041f2cab9dce90bea758a1433a5948154f126cc
Laura Lindzey <lindzey@gmail.com>
2013-12-10 12:58:06 -0800

    adding clear_table script that works with ORK

--- clear_table/setup.py
+++ clear_table/setup.py
@@ -5,7 +5,8 @@ d = generate_distutils_setup(
     packages=['clear_table'],
     package_dir={'': 'src'},
     requires=['rospy', 'moveit_msgs', 'sensor_msgs', 'shape_msgs', 'shared_autonomy_msgs', 'smach', 'smach_ros', 'cluster_grasp_planner'],
-    scripts=['scripts/clear_table_moveit.py']
+    scripts=['scripts/clear_table_moveit.py',
+             'scripts/clear_table_moveit_ork.py']
 )
 
 setup(**d)
